36bba308d108690c7cfd1f2cc95a5e039eea38a6
Yongsu Park <yongsu@Yongsus-MacBook-Air.local>
2014-10-07 16:35:08 +0900

    1st commit

---
+++ breakout/Sprite.java
@@ -0,0 +1,8 @@
+package breakout;
+
+import java.awt.Image;
+import java.awt.Rectangle;
+
+public class Sprite {
+
+  
---
+++ collision/Alien.java
@@ -0,0 +1,59 @@
+package collision;
+
+import java.awt.Image;
+import java.awt.Rectangle;
+
+import javax.swing.ImageIcon;
+
+
+public class Alien {
+  private String craft = "../images/alien.png";
+
+  private int x;
+  private int y;
+  private int width;
+  private int height;
+  private boolean visible;
+  private Image image;
+
+  public Alien(int x, int y) {
+    ImageIcon ii = new ImageIcon(this.getClass().getResource(craft));
+    image = ii.getImage();
+    width = image.getWidth(null);
+    height = image.getHeight(null);
+    visible = true;
+    this.x = x;
+    this.y = y;
+  }
+
+  public void move() {
+    if ( x < 0 )
+      x = 400;
+    x-=1;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public Image getImage() {
+    return image;
+  }
+
+  public void setVisible(boolean visible) {
+    this.visible = visible;
+  }
+
+  public boolean isVisible() {
+    return visible;
+  }
+
+  public Rectangle getBounds() {
+    return new Rectangle(x, y, width, height);
+  }
+
+}
